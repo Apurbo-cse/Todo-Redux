a2fa46263c7060f8a7321b75dc42d63fa0416d53
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-20 15:44:12 +0600

    ADDED

--- src/redux/todos/reducer.jsx
+++ src/redux/todos/reducer.jsx
@@ -1,4 +1,25 @@
+
+import { initialState } from "./initialState";
+import { ADDED } from "./actionType";
+
+function nextTodoId = (todos) => {
+    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+    return maxId + 1;
+}
+
 const reducer = (state = initialState, action) => {
 
+    switch (action.type) {
+        case ADDED:
+            return [
+                ...state, 
+                {
+                    id: nextTodoId(state)
+                }
+            ]
+    
+        default:
+            break;
+    }
 
 }
