68b149e19daae6ed332b1eea02dd137272281f80
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-20 15:29:37 +0600

    status

--- src/redux/filters/actions.jsx
+++ src/redux/filters/actions.jsx
@@ -1,10 +1,20 @@
-import { COLOR } from "./actionType"
+import { COLOR, STATUS } from "./actionType"
 
 export const color = (color, type) => {
     return {
         type: COLOR,
         payload : {
             color, type
+        }
+    }
+}
+
+
+export const status = (status) => {
+    return {
+        type: STATUS,
+        payload : {
+            status,
         }
     }
 }
