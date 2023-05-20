b051af5c535652427e0b42a5a3257d2a7c2ba9c8
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-20 15:20:50 +0600

    toggled

--- src/redux/todos/actions.jsx
+++ src/redux/todos/actions.jsx
@@ -1,8 +1,15 @@
-import { ADDED } from "./actionType"
+import { ADDED, TOGGLED } from "./actionType"
 
 export const added = (todoText) => {
     return {
         type: ADDED,
         payload : todoText
+    }
+}
+
+export const toggled = (todoId) => {
+    return {
+        type: TOGGLED,
+        payload : todoId
     }
 }
