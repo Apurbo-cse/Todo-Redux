c044ef7bc1f17f1818ce0344cb7efa8f20e9a5c4
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-20 17:58:49 +0600

    Update reducer.jsx

--- src/redux/todos/reducer.jsx
+++ src/redux/todos/reducer.jsx
@@ -25,7 +25,7 @@ const reducer = (state = initialState, action) => {
                 }
                  return {
                         ...todo,
-                        completed: !completed
+                        completed: !todo.completed
                  }
             })
 
