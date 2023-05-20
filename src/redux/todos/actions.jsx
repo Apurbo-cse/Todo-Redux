ccdc81ae29f2f145b4bc7be03d5232d6f1cdc447
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-20 15:23:13 +0600

    deleted

--- src/redux/todos/actions.jsx
+++ src/redux/todos/actions.jsx
@@ -1,4 +1,4 @@
-import { ADDED, COLOR, TOGGLED } from "./actionType"
+import { ADDED, COLOR, DELETED, TOGGLED } from "./actionType"
 
 export const added = (todoText) => {
     return {
@@ -20,5 +20,13 @@ export const color = (todoId, color) => {
         payload : {
              todoId , color
         }
+    }
+}
+
+
+export const deleted = (todoId) => {
+    return {
+        type: DELETED,
+        payload :todoId 
     }
 }
