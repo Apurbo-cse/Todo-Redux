a3a416b514089ece5dbc6818bb5fddb5ffdc4525
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-20 15:17:16 +0600

    actionType

---
+++ src/redux/todos/actionType.jsx
@@ -0,0 +1,6 @@
+export const ADDED = "todos/added";
+export const TOGGLED = "todos/toggled";
+export const COLOR = "todos/color";
+export const DELETED = "todos/deleted";
+export const COMPLETED = "todos/completed";
+export const CLEAR = "todos/clear";
